51ca2cd64064fb0d5e365743d088a342a53ff747
Filipe Mota <fsmota@ic.ufal.br>
2023-10-11 14:36:37 -0300

    :construction: feat: Login and logout methods

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,38 +1,34 @@
-import { AiOutlineCalendar, AiFillAlert } from "react-icons/ai";
 import Button from "../../components/Button";
-import TestCard from "../../components/TestCard";
-import ListCard from "../../components/ListCard";
-import { useEffect, useState } from "react";
+import { useState, FormEvent, useEffect } from "react";
 import instance from "../../axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/Input";
 import logo from "../../assets/logo.png";
 
-type TestCard = {
-    id: string;
-    name: string;
-    description: string;
-    dateStart: string;
-}
 
 function Login() {
-    const id = "014f79e8-b891-4329-85a5-bb54ba5692ea"
-    const [email, setEmail] = useState<string>('');
-    const [pass, setPass] = useState<string>('');
+
+    const navigate = useNavigate();
+    const [loggedIn, setLoggedIn] = useState<boolean>(false)
+    const [cpf, setCpf] = useState<string>('');
+    const [password, setPass] = useState<string>('');
     
+    function submit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        instance.post("/users/auth/login", { cpf, password})
+            .then((res)=> {
+               console.log(res);
+               sessionStorage.setItem("token", res.data.token);
+               setLoggedIn(true)
+            })
+            .catch(console.log)
+    }
+
     useEffect(() => {
-      return () => {
-        instance.get(`/users/${id}/tests`)
-        .then(
-            res => {
-                console.log(res.data);
-            }
-        )
-        .catch((err) => {
-            console.log(err);
-        })
-      }
-    }, [id])
+        if (sessionStorage.getItem("token") && loggedIn)
+           navigate("/")
+    },[loggedIn, navigate])
+
     return (
         <>
             <div className="bg-Blue w-full h-screen p-3">
@@ -40,14 +36,16 @@ function Login() {
                     <div>
                         <img src={logo} width={500} className="mb-4"/>
                         <div className="bg-white w-full rounded-xl p-8 justify-center content-center">
-                            <div className="mb-2">
-                                <Input placeholder="Email" type='email' onChange={(ev) => setEmail(ev.target.value)} value={email}/>
-                                <Input placeholder="Senha" type='password' onChange={(ev)=> setPass(ev.target.value)} value={pass}/>
-                            </div>
-                            <Link className="text-xs text-slate-500" to={"#"}>Esqueceu a senha?</Link>
-                            <div className="mt-3">
-                                <Button size="w-full">Entrar</Button>
-                            </div>
+                            <form method="post" onSubmit={submit}>
+                                <div className="mb-2">
+                                    <Input placeholder="CPF" type='text' onChange={(ev) => setCpf(ev.target.value)} value={cpf}/>
+                                    <Input placeholder="Senha" type='password' onChange={(ev)=> setPass(ev.target.value)} value={password}/>
+                                </div>
+                                <Link className="text-xs text-slate-500" to={"#"}>Esqueceu a senha?</Link>
+                                <div className="mt-3">
+                                    <Button size="w-full" type="submit">Entrar</Button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                </div>
