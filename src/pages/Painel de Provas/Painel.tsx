c69dc954f04ae67a8ccbe9c9cdfa594b513be05f
Filipe Mota <fsmota@ic.ufal.br>
2023-10-16 14:19:04 -0300

    :construction: feat: login formmater fields

--- src/pages/Painel de Provas/Painel.tsx
+++ src/pages/Painel de Provas/Painel.tsx
@@ -1,10 +1,10 @@
-import { AiOutlineCalendar, AiFillAlert } from "react-icons/ai";
+import { AiOutlineCalendar, AiFillAlert, AiOutlineCheckCircle, AiOutlineCloseCircle } from "react-icons/ai";
 import Button from "../../components/Button";
 import TestCard from "../../components/TestCard";
 import ListCard from "../../components/ListCard";
 import { useEffect, useState } from "react";
 import instance from "../../axios";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 type TestCard = {
     id: string;
@@ -16,6 +16,7 @@ type TestCard = {
 function Painel() {
     const id = "014f79e8-b891-4329-85a5-bb54ba5692ea"
     const [tests, setTests] = useState<TestCard[]>([]);
+    const navigate = useNavigate();
     
     useEffect(() => {
       return () => {
@@ -33,19 +34,41 @@ function Painel() {
     }, [id])
     return (
         <>
+            <div className="px-6">
+            <h2 className="text-Blue text-lg font-bold uppercase">PROVAS AGENDADAS</h2>
             <ListCard>
                 {
                     tests.map(test => (
-                        <Link to={`details/${test.id}`}>
-                            <TestCard handleClick={console.log}
-                            title={test.name}
-                            subText={test.dateStart} 
-                            description={test.description}
-                            icon={AiOutlineCalendar} />
-                        </Link>
+                        <TestCard handleClick={() => navigate("./aboutTest")}
+                        icon={AiOutlineCalendar}
+                        title={test.name}
+                        subText={test.dateStart} 
+                        description={test.description}
+                        />
                     ))
                 }
             </ListCard>
+            <h2 className="text-Blue text-lg font-bold uppercase">Provas Realizadas</h2>
+            <ListCard>
+                <TestCard 
+                    variant="confirm"
+                    handleClick={() => navigate("./aboutTest")}
+                    icon={AiOutlineCheckCircle}
+                    title="Supletivo" 
+                    subText="12/10/1993" 
+                    description="Exame de aprovação do candidato no programa Vem Que Dá Tempo. Exame de aprovação do candidato no programa Vem Que Dá Tempo."
+                />
+
+                <TestCard 
+                    variant="alert"
+                    handleClick={console.log}
+                    icon={AiOutlineCloseCircle}
+                    title="Supletivo" 
+                    subText="12/10/1993" 
+                    description="Exame de aprovação do candidato no programa Vem Que Dá Tempo."
+                />
+            </ListCard>
+        </div>
             <Button variant="solid" color="warning">
                 <AiFillAlert />
                 ATENÇÃO
