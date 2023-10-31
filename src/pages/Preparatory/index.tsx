5952e93e7386bbef4f92c94cb036580ebfea02a4
davidsonaguiar <davidson.05@outlook.com>
2023-10-31 09:55:45 -0300

    :sparkles: table e modal

--- src/pages/Preparatory/index.tsx
+++ src/pages/Preparatory/index.tsx
@@ -1,8 +1,26 @@
-import SubjectCard from "../../components/SubjectCard";
-
+//import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material";
+import Table from "../../components/Table"
 function Preparatory() {
-  return(
-    <SubjectCard subject="Matemática" total="10" done="3"/>
+
+  const tableRegisters = [
+    {
+      id: "001",
+      name: "davidson",
+      sobrenome: "aguiar",
+      dataNascimento: "12/10/1993"
+    },
+    {
+      id: "002",
+      name: "agatha",
+      sobrenome: "aguiar",
+      dataNascimento: "22/11/2014"
+    }
+  ]  
+
+  return (
+    <div>
+      <Table registros={tableRegisters}/>
+    </div>
   );
 }
 
